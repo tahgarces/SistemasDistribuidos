f9e6a48416d431b05c44b47111799fce5d94e38e
Taynara Garces de Lima <taynaragarces@gmail.com>
2019-04-29 18:49:54 -0300

    davi bugando o codigo

--- mensalidade/src/main/java/com/atividade/teste/Teste.java
+++ mensalidade/src/main/java/com/atividade/teste/Teste.java
@@ -1,20 +1,44 @@
 package com.atividade.teste;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import com.atividade.entidade.Aluno;
 import com.atividade.entidade.Mensalidade;
 
 
 public class Teste {
-
+	
+	public static List<Mensalidade> verificaAtraso() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("teste");		
+		EntityManager em = emf.createEntityManager();
+		
+		TypedQuery query = em.createQuery("select m from mensalidade as m where m.statusPagamento=true", Mensalidade.class);
+		//List<Mensalidade> listaMensalidade = new
+		
+		List<Mensalidade> listaEmAberto =query.getResultList();
+		List<Mensalidade> listaEmAtraso = new ArrayList();
+		for(int i = 0; i<listaEmAberto.size(); i++) {
+			System.out.printf("\n1");
+			if(listaEmAberto.get(i).getDataVencimento().before(LocalDate.now()))
+				System.out.println("Ok");
+				listaEmAtraso.add(listaEmAberto.get(i));
+			}
+		return listaEmAtraso;
+		}
+		
+	
 	public static void main(String[] args) {
+
 					
-		Calendar cal = Calendar.getInstance();
+		/*Calendar cal = Calendar.getInstance();
 		cal.set(Calendar.YEAR, 2018);
 		cal.set(Calendar.MONTH, Calendar.DECEMBER);
 		cal.set(2019, 2, 13);	
@@ -25,14 +49,18 @@ public class Teste {
 		Aluno aluno2 = new Aluno("Maria", "taynaragarces@gmail.com", "98984018000");
 		Mensalidade mensalidade = new Mensalidade(aluno2, cal, cal);
 		
+		
+		
 		em.getTransaction().begin();
 		em.persist(aluno2);
-
 		em.persist(mensalidade);
 		em.getTransaction().commit();
-		emf.close();
+		emf.close();*/
 		
+	
+		System.out.println(verificaAtraso().get(1));
 		
+	
 		
 	}
 
